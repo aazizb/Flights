9a6567a6420794c58543e739c41aa4a6f1f676c2
aazizb <aazizb@hotmail.com>
2023-06-26 20:17:55 +0200

    Injected as DI repositorymanager

--- WebApi/Extensions/IServiceCollectionExtensions.cs
+++ WebApi/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,8 @@
 
 using LoggerService;
 
+using Repository;
+
 namespace WebApi.Extensions
 {
     public static class IServiceCollectionExtensions
@@ -20,10 +22,15 @@ namespace WebApi.Extensions
         {
             services.AddSingleton<ILoggerManager, LoggerManager>();
         }
+        private static void ConfigureRepositoryManager(this IServiceCollection services)
+        {
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        }
         public static void AddWebApi(this IServiceCollection services)
         {
             services.ConfigureCors();
             services.ConfigureLoggerService();
+            services.ConfigureRepositoryManager();
         }
     }
 }
