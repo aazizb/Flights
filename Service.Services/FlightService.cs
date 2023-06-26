023ba56f17e463bda242d341c5c23e9b804d03c5
aazizb <aazizb@hotmail.com>
2023-06-26 20:49:40 +0200

    Implemented flightservice and servicemanager

---
+++ Service.Services/FlightService.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+
+using Service.Contracts;
+
+namespace Service.Services
+{
+    public sealed class FlightService : IFlightService
+    {
+        private readonly IRepositoryManager repository;
+        private readonly ILoggerManager logger;
+
+        public FlightService(IRepositoryManager repository, ILoggerManager logger)
+        {
+            this.repository = repository;
+            this.logger = logger;
+        }
+
+    }
+}
---
+++ Service.Services/ServiceManager.cs
@@ -0,0 +1,19 @@
+﻿using Contracts;
+
+using Service.Contracts;
+
+namespace Service.Services
+{
+    public sealed class ServiceManager : IServiceManager
+    {
+        private readonly Lazy<IFlightService> flightService;
+
+        public ServiceManager(IRepositoryManager repository, ILoggerManager logger)
+        {
+            flightService = new Lazy<IFlightService>(() => new
+            FlightService(repository, logger));
+        }
+        public IFlightService FlightService =>
+            flightService.Value;
+    }
+}
