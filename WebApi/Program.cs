9dbc6ef4b48b1efa91bbaf392d02a2484229f1d4
aazizb <aazizb@hotmail.com>
2023-06-27 06:05:29 +0200

    Added presentation layer

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -5,7 +5,11 @@ using WebApi.Extensions;
 var builder = WebApplication.CreateSlimBuilder(args);
 {
     LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.config"));
+
     builder.Services.AddWebApi(builder.Configuration);
+
+    builder.Services.AddControllers()
+        .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
 
 }
 
